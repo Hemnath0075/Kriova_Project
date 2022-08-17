2bf0f80341612a82fb52a1251694265c8c057eca
Hemnath0075 <hemnath5700.com>
2022-08-17 23:33:43 +0530

    client basic code with Protected Route

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { useState } from 'react';
+import { BrowserRouter as Router,Link,Route,Switch } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import ProtectedRoute from './ProtectedRoute';
+
+function App() {
+  const [isAuth, setIsAuth]=useState(false);
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/signin" exact component={Login}></Route>
+          <ProtectedRoute path="/home" component={Home} isAuth={isAuth} />
+          </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
